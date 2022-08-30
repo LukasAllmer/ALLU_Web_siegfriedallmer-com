6e7c828736b090ef0dfb15158890090687849287
Lukas Allmer <lukas@allmer.org>
2022-08-30 17:54:02 +0200

    Finished first layout of Website

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
-// This is empty for now
+"use strict";
+
+// Insert correct date for copyright-notice.
+
+var copyrightNotice = document.getElementById('copyrightNotice');
+var currentYear = new Date().getFullYear();
+copyrightNotice.innerHTML = `Copyright © ${currentYear} Siegfried Allmer. Alle Rechte vorbehalten.`;
+
